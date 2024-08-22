f2fbf88e8152d6e7db840a8be42ddec84e53fe4e
YaroslavBelinskyiMoC <yaroslav.belinskyi@masterofcode.com>
2024-08-22 23:32:47 +0300

    db connection and embed inital setup

--- src/mock/mock_article.ts
+++ src/mock/mock_article.ts
@@ -1,56 +1,7 @@
 const articles = [
   {
-    "Introduction": {
-      "Overview": "Zipify Pages is a powerful tool that can help you create beautiful, high-converting pages for your Shopify store. This drag-and-drop page builder allows you to create custom pages without any coding experience or need for a developer.",
-      "Features": [
-        "Choose from proven profitable templates or create a page completely from scratch by adding text, images, videos, and other elements to your pages with a variety of pre-set sections.",
-        "Easily customize the look and feel of your pages with a variety of fonts, colors, and backgrounds."
-      ],
-      "PageTypes": [
-        "Home Page",
-        "Pre-sell pages",
-        "Lead magnet pages",
-        "Product offer pages",
-        "Holiday sales pages",
-        "Special Offer pages",
-        "A fully functional store Blog"
-      ],
-      "CustomizationOptions": "You're able to create all of these types of pages from scratch using our wide variety of page sections or by using one of our amazing pre-built page templates."
-    },
-    "Benefits": [
-      "No coding experience required: Zipify Pages is a drag-and-drop page builder, so you don't need any coding experience to create custom pages for your Shopify store.",
-      "A variety of templates and sections: Zipify Pages offers a variety of templates and sections to get you started, and you can customize the look and feel of your pages with a variety of fonts, colors, and backgrounds.",
-      "High-converting features: Zipify Pages offers features like dynamic product showcases, countdown timers, and A/B testing.",
-      "Mobile responsiveness: Your pages will look great on any device and can be designed, edited, and previewed from a mobile perspective.",
-      "World Class Support: Get help from real ecom experts who run 8 & 9-figure stores."
-    ],
-    "Glossary": {
-      "ABTesting": "A method of comparing two versions of a page to determine which one performs better.",
-      "AddToCartButton": "A specific type of button that adds products to the customer’s cart and can redirect them to other pages, or leave them on the same page.",
-      "Analytics": "Data and insights about your page’s performance, such as traffic, conversion rate, and bounce rate.",
-      "Sections": "Pre-designed page sections that you can easily add to your page. Examples of sections include a header, a product showcase, and a testimonial section.",
-      "BuyBox": "The area of a page where customers can add products to the cart, view product images, and read information about the assigned product.",
-      "CallToAction": "A message that encourages visitors to take a specific action, such as clicking a button or signing up for a newsletter.",
-      "ConversionRate": "The percentage of visitors who take a desired action on your page, such as making a purchase or filling out a form.",
-      "CustomCode": "The ability to add your own HTML, CSS, or JavaScript code to your pages for more advanced customization.",
-      "Elements": "The building blocks of a page, such as text boxes, images, buttons, and forms.",
-      "Footer": "The bottom of a page. Usually contains copyright information, contact details, navigation links, etc.",
-      "Funnels": "A series of interconnected pages designed to guide customers through the buying process.",
-      "GlobalVsLocal": "Global scripts are applied to all pages, and Local scripts are applied on a per-page basis.",
-      "Header": "The Header element represents a container for introductory content or a set of navigational links. Typically contains the store’s logo or icon, a search bar, or a link to the Cart page.",
-      "HeroSection": "The main section of a landing page that contains the most important information and call-to-action.",
-      "Integrations": "Connections between Zipify Pages and other tools and platforms, such as email marketing services or payment gateways.",
-      "MobileResponsiveness": "The ability of a page to adjust its layout and content to fit different screen sizes and devices.",
-      "Pages": "The individual landing pages you create using Zipify Pages.",
-      "Scripts": "A list of programmatically-written instructions that can be carried out on command.",
-      "Sections_2": "A collection of elements that form a single block of content on your page.",
-      "Styles": "Styles determine how elements of a page look (text size, fonts, colors, etc.).",
-      "Template": "A pre-designed page layout that you can use as a starting point for your own page."
-    }
-  },
-  {
-    "article": `
-      Introduction\n\n
+    pageContent: `
+      Introduction
 
       [Zipify Pages](https://help.zipify.com/en/articles/7889067-about-zipify-pages) is a powerful tool that can help you create beautiful, high-converting pages for your Shopify store. This drag-and-drop page builder allows you to create custom pages without any coding experience or need for a developer.
 
@@ -98,7 +49,8 @@ const articles = [
       - Sections: A collection of elements that form a single block of content on your page.
       - Styles: Styles determine how elements of a page look (text size, fonts, colors, etc.).
       - Template: A pre-designed page layout that you can use as a starting point for your own page.
-      `
-  }
-]
+      `,
+    metadata: { a: 1 },
+  },
+];
 export default articles;
