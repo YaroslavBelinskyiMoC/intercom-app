0563b8e82eed7801b43b92a4a0cbb695103cd143
YaroslavBelinskyiMoC <yaroslav.belinskyi@masterofcode.com>
2024-08-30 12:13:07 +0300

    star updating gemerator

--- src/routes/intercom.ts
+++ src/routes/intercom.ts
@@ -1,48 +1,54 @@
-import { Router, Request, Response } from 'express';
-import { initialCanvas, createFinalCanvas, textCanvas, requestCanvas, additionalAnswerGenerator } from '../components';
-import { logger } from '../logger';
+import { Router, Request, Response } from "express";
+import {
+  initialCanvas,
+  createFinalCanvas,
+  textCanvas,
+  requestCanvas,
+  additionalAnswerGenerator,
+} from "../components";
+import { logger } from "../logger";
+import { dbClient } from "../services/db";
 
 const log = logger(__filename);
 
 const router: Router = Router();
 
-router.post('/initialize', (request: Request, response: Response) => {
-    try {
-        log.info('App initialization');
-        response.send(requestCanvas);
-    } catch (error) {
-        log.info(error);
-        response.status(500).send({ error: "Internal Server Error" });
-    }
+router.post("/initialize", (request: Request, response: Response) => {
+  try {
+    log.info("App initialization");
+    response.send(requestCanvas);
+  } catch (error) {
+    log.info(error);
+    response.status(500).send({ error: "Internal Server Error" });
+  }
 });
 
-router.post('/submit', (request: Request, response: Response) => {
-    try {
-        log.info('Submit clicked');
-        if (request.body.component_id == 'submit_button') {
-            // let department = request.body.input_values.departmentChoice;
-            console.log(request.body.input_values);
-            response.send(textCanvas);
-        } else if (request.body.component_id == 'submit-issue-form') {
-            // let department = request.body.input_values.departmentChoice;
-            console.log(request.body.current_canvas.content.components);
-            const canvasAnswer = {
-                canvas: {
-                    content: request.body.current_canvas.content
-                }
-            }
-            const array = canvasAnswer.canvas.content.components;
-            const input = request.body.input_values.description;
-            const newComponents = additionalAnswerGenerator(input);
-            array.splice(array.length - 12, 0, ...newComponents);
-            response.send(canvasAnswer);
-        } else {
-            response.send(requestCanvas);
-        }
-    } catch (error) {
-        log.info(error);
-        response.status(500).send({ error: "Internal Server Error" });
+router.post("/submit", async (request: Request, response: Response) => {
+  try {
+    log.info("Submit clicked");
+    if (request.body.component_id == "submit_button") {
+      console.log(request.body.input_values);
+      const gptAnswer = await dbClient.search(request.body.input_values);
+      response.send(textCanvas);
+    } else if (request.body.component_id == "submit-issue-form") {
+      console.log(request.body.current_canvas.content.components);
+      const canvasAnswer = {
+        canvas: {
+          content: request.body.current_canvas.content,
+        },
+      };
+      const array = canvasAnswer.canvas.content.components;
+      const input = request.body.input_values.description;
+      const newComponents = additionalAnswerGenerator(input);
+      array.splice(array.length - 12, 0, ...newComponents);
+      response.send(canvasAnswer);
+    } else {
+      response.send(requestCanvas);
     }
+  } catch (error) {
+    log.info(error);
+    response.status(500).send({ error: "Internal Server Error" });
+  }
 });
 
 export default router;
